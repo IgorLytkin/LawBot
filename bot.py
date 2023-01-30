aa5213657269cf23cfa7fc3c7070c3062fe833e2
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-01-31 06:29:57 +0700

    знакомство с aiogram

--- bot.py
+++ bot.py
@@ -27,6 +27,29 @@ async def cmd_test1(message: types.Message):
 async def cmd_test2(message: types.Message):
     await message.reply("Test 2")
 
+@dp.message(commands=["answer"])
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+
+@dp.message(commands=["reply"])
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+
+@dp.message(commands=["dice"])
+async def cmd_dice(message: types.Message, bot: Bot):
+    await bot.send_dice(os.getenv("LAWCHANNEL_BOT_ID"), emoji="🎲")
+
+@dp.message(commands=["add_to_list"])
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(commands=["show_list"])
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
